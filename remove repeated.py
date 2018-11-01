a4d4e2e1962dd3e69201ad12e8cfe3076ef42ced
neurophilosophy <35268511+neurophilosophy@users.noreply.github.com>
2018-11-01 19:17:45 +0800

    Delete remove repeated.py

--- remove repeated.py
+++
@@ -1,7 +0,0 @@
-blank = []
-list1 = [7,' ',' ',' ',' ',' ',' ','    ','','  ',' ',' ',' ',' ',' ',' ', 9,' ']
-for j in list1:
-    while list1.count(j)>=1 and not j in blank and j != '' :
-        print(j)
-        blank.append(j)
-        print(blank)
